1b7b147ae507625057f8ea4c477a921937c94bf1
vincentpchevalier <vincentpchevalier@proton.me>
2024-05-20 22:03:18 -0400

    Clean up script

--- main.js
+++ main.js
@@ -2,31 +2,32 @@ import NetworkError from './utils.js';
 
 // Global variables
 const userData = []; // Single source of truth
-let scrollMode = false;
+const maxUsers = 0; // Max number of users we can fetch
+let scrollMode = true;
+
+// Intersection Observer API
 let footerObserver;
 
-const maxUsers = 50; // Max number of users we can fetch
+const options = {
+	root: null,
+	rootMargin: '0px',
+	threshold: 0.15,
+};
 
+// DOM elements
 const footer = document.querySelector('footer');
 const loadMoreBtn = document.getElementById('load-more');
+const toggleScrollModeBtn = document.getElementById('toggle-scroll-mode');
 
 function init() {
 	console.log('App initialized, fetching users...');
 
-	// Intersection Observer API - setup footer observer
-	const options = {
-		root: null,
-		rootMargin: '0px',
-		threshold: 0.15,
-	};
-
+	// Instantiate observer for the footer and start observing
 	footerObserver = new IntersectionObserver(revealMoreUsers, options);
-	// footerObserver.observe(footer);
+	footerObserver.observe(footer);
 
 	// Event listener for the toggle scroll mode button
-	document
-		.getElementById('toggle-scroll-mode')
-		.addEventListener('click', toggleScrollMode);
+	toggleScrollModeBtn.addEventListener('click', toggleScrollMode);
 
 	// Event listener for the load more button
 	loadMoreBtn.addEventListener('click', () => {
@@ -35,42 +36,54 @@ function init() {
 	});
 
 	// Fetch users
-	// fetchUsers(10);
+	fetchUsers(10);
 }
 
 async function fetchUsers(size = 20) {
+	let maxUsersReached = userData.length >= maxUsers;
+	console.log('max users:', maxUsers, 'maxUsersReached:', maxUsersReached);
+
 	const url = `https://random-data-api.com/api/v2/users?size=${size}`;
-	try {
-		const response = await fetch(url);
-		console.log(response.ok, response.status, response.statusText);
-		if (!response.ok) {
-			throw new NetworkError(
-				`Response failed with status ${response.status}`,
-				response
-			);
-		}
-		const data = await response.json();
-		const users = data.map(
-			({ id, avatar, first_name, last_name, username, email }) => {
-				return {
-					id,
-					avatar,
-					name: `${first_name} ${last_name}`,
-					username,
-					email,
-				};
+
+	// Fetch users but limit the number of users to maxUsers because we don't want to overload our fetch requests to a free API (Error 429 - Too Many Requests)
+	if (!maxUsersReached) {
+		try {
+			const response = await fetch(url);
+			if (!response.ok) {
+				throw new NetworkError(
+					`Response failed with status ${response.status}`,
+					response
+				);
 			}
-		);
-		userData.push(...users);
-	} catch (err) {
-		if (err instanceof NetworkError) {
-			if (userData.length === 0) showSnackbar(err.message);
-		} else {
-			showSnackbar('An error occurred. Please try again.');
+			const data = await response.json();
+			const users = data.map(
+				({ id, avatar, first_name, last_name, username, email }) => {
+					return {
+						id,
+						avatar,
+						name: `${first_name} ${last_name}`,
+						username,
+						email,
+					};
+				}
+			);
+			userData.push(...users);
+		} catch (err) {
+			if (err instanceof NetworkError) {
+				if (userData.length === 0) showSnackbar(err.message);
+			} else {
+				showSnackbar('An error occurred. Please try again.');
+			}
 		}
+		loadUsers(userData);
+	} else {
+		showSnackbar(
+			`That's the max number of users we can fetch at this time.`,
+			'success'
+		);
+		loadMoreBtn.disabled = true;
+		loadMoreBtn.classList.add('btn--disabled');
 	}
-	console.log(userData.length);
-	loadUsers(userData);
 }
 
 function loadUsers(users) {
@@ -109,13 +122,8 @@ function revealMoreUsers(entries) {
 
 	if (!entry.isIntersecting) return; // Guard clause
 
-	// Fetch more users but limit the number of users to 90 because we don't want to overload our fetch requests to a free API (Error 429 - Too Many Requests)
-	if (userData.length <= maxUsers) {
-		console.log('Fetching more users');
-		fetchUsers(10);
-	} else {
-		console.log("That's the max number of users we can fetch at this time.");
-	}
+	// Fetch more users
+	if (userData.length !== 0) fetchUsers(10);
 }
 
 function toggleScrollMode() {
@@ -123,10 +131,16 @@ function toggleScrollMode() {
 	console.log('scrollMode:', scrollMode);
 
 	// Show message in snackbar about the scroll mode
+	showSnackbar(
+		`Scroll mode is ${
+			scrollMode ? 'enabled. Now fetching data.' : 'disabled'
+		}.`,
+		'success'
+	);
 
 	// Disable the load more button when scroll mode is enabled and we've reached the max number of users
 	loadMoreBtn.disabled = scrollMode && userData.length >= maxUsers; // && is a short-circuit operator that only evaluates the second expression if the first one is true
-	loadMoreBtn.classList.toggle('btn--disabled', !scrollMode); // second argument is a boolean that determines whether the class should be added or removed
+	loadMoreBtn.classList.toggle('btn--disabled', !scrollMode || maxUsersReached); // second argument is a boolean that determines whether the class should be added or removed
 
 	scrollMode
 		? footerObserver.observe(footer)
@@ -150,9 +164,9 @@ function showSnackbar(message, type = 'error') {
 }
 
 // 1. On page load, fetch the data from the API : https://random-data-api.com/api/v2/users?size=${size}
-// 2. Retrieve 30 users from the API, display them as cards in the UI
-// 3. Each card should have the following details: Name (first_name + last_name), username, email, avatar, and an id.
-// 4. As the user scrolls down, fetch 10 more users and display them in the UI. Use observer to detect when the user has scrolled to the bottom of the page. Only display the cards once their avatars have been loaded.
+// 2. Retrieve users from the API. Limit the number of users using a maxUsers variable.
+// 3. Display them as cards in the UI. Each card should have the following details: Name (first_name + last_name), username, email, avatar, and an id.
+// 4. As the user scrolls down, fetch more users and display them in the UI. Use observer to detect when the user has scrolled to the bottom of the page. Only display the cards once their avatars have been loaded.
 // 5. Show an empty flashing card animation as a loading animation at the bottom of the page when the user scrolls down and the data is being fetched.
 // 6. When the user clicks on the Load Manually, turn off the auto loading of users when the user scrolls down. Show the Load More button at the bottom of the page. When the user clicks on the Load More button, fetch the next 10 users and display them in the UI.
 // 7. Create a custom error handling mechanism to handle the API errors. Extend the Error class and create a custom error class. Use this custom error class to handle the API errors.
